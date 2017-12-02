4f5fa542306187654dbc5fb9e7b60a63bad435a0
ElegantGod <838060390@qq.com>
2017-12-02 15:40:26 +0800

    Signed-off-by: ElegantGod <838060390@qq.com>

--- src/runtracker.cpp
+++ src/runtracker.cpp
@@ -8,11 +8,46 @@
 
 #include "kcftracker.hpp"
 
-#include <dirent.h>
+//#include <dirent.h>
 
 using namespace std;
 using namespace cv;
 
+Mat frame;
+Mat frameCopy;
+bool leftButtonDownFlag = false;
+Point originalPoint; //矩形框起点  
+Point processPoint; //矩形框终点 
+bool bInit = false;
+void onMouse(int event,int x,int y,int flags,void *ustc)    
+{       
+	frameCopy=frame.clone();  
+    if(event==CV_EVENT_LBUTTONDOWN)    
+    {    
+        leftButtonDownFlag=true; //标志位  
+        originalPoint=Point(x,y);  //设置左键按下点的矩形起点  
+        processPoint=originalPoint;  
+		bInit = false;
+    }    
+    if(event==CV_EVENT_MOUSEMOVE&&leftButtonDownFlag)    
+    {    
+        processPoint=Point(x,y);  
+        if(originalPoint!=processPoint)  
+        {  
+            //在复制的图像上绘制矩形  
+            rectangle(frameCopy,originalPoint,processPoint,Scalar(255,0,0),2);  
+        }  
+        imshow("KCF",frameCopy);  
+    }    
+    if(event==CV_EVENT_LBUTTONUP)    
+    {    
+        leftButtonDownFlag=false; 
+		imshow("KCF",frameCopy);  
+    }      
+} 
+
+#define WIDTH 640
+#define HEIGHT 360
 int main(int argc, char* argv[]){
 
 	if (argc > 5) return -1;
@@ -20,7 +55,7 @@ int main(int argc, char* argv[]){
 	bool HOG = true;
 	bool FIXEDWINDOW = false;
 	bool MULTISCALE = true;
-	bool SILENT = true;
+	bool SILENT = false;
 	bool LAB = false;
 
 	for(int i = 0; i < argc; i++){
@@ -44,58 +79,17 @@ int main(int argc, char* argv[]){
 	KCFTracker tracker(HOG, FIXEDWINDOW, MULTISCALE, LAB);
 
 	// Frame readed
-	Mat frame;
 
 	// Tracker results
 	Rect result;
-
-	// Path to list.txt
-	ifstream listFile;
-	string fileName = "images.txt";
-  	listFile.open(fileName);
-
-  	// Read groundtruth for the 1st frame
-  	ifstream groundtruthFile;
-	string groundtruth = "region.txt";
-  	groundtruthFile.open(groundtruth);
-  	string firstLine;
-  	getline(groundtruthFile, firstLine);
-	groundtruthFile.close();
-  	
-  	istringstream ss(firstLine);
-
-  	// Read groundtruth like a dumb
-  	float x1, y1, x2, y2, x3, y3, x4, y4;
-  	char ch;
-	ss >> x1;
-	ss >> ch;
-	ss >> y1;
-	ss >> ch;
-	ss >> x2;
-	ss >> ch;
-	ss >> y2;
-	ss >> ch;
-	ss >> x3;
-	ss >> ch;
-	ss >> y3;
-	ss >> ch;
-	ss >> x4;
-	ss >> ch;
-	ss >> y4; 
+	namedWindow("KCF");
+	setMouseCallback("KCF",onMouse);
 
 	// Using min and max of X and Y for groundtruth rectangle
-	float xMin =  min(x1, min(x2, min(x3, x4)));
-	float yMin =  min(y1, min(y2, min(y3, y4)));
-	float width = max(x1, max(x2, max(x3, x4))) - xMin;
-	float height = max(y1, max(y2, max(y3, y4))) - yMin;
-
-	
-	// Read Images
-	ifstream listFramesFile;
-	string listFrames = "images.txt";
-	listFramesFile.open(listFrames);
-	string frameName;
-
+	int x;//min(x1, min(x2, min(x3, x4)));
+	int y;//min(y1, min(y2, min(y3, y4)));
+	int width;//max(x1, max(x2, max(x3, x4))) - xMin;
+	int height;//max(y1, max(y2, max(y3, y4))) - yMin;
 
 	// Write Results
 	ofstream resultsFile;
@@ -105,35 +99,44 @@ int main(int argc, char* argv[]){
 	// Frame counter
 	int nFrames = 0;
 
-
-	while ( getline(listFramesFile, frameName) ){
-		frameName = frameName;
-
+	VideoCapture vc("/disk1/chengw/video/264a.mp4");
+	if(!vc.isOpened())  
+    {  
+        return -1;  
+    }  
+	while ( 1 )
+	{
+		if(!leftButtonDownFlag)
+			vc.read(frame);
+		if(frame.rows != HEIGHT ||frame.cols != WIDTH)
+        {
+			resize(frame,frame, Size(WIDTH, HEIGHT));
+        }
 		// Read each frame from the list
-		frame = imread(frameName, CV_LOAD_IMAGE_COLOR);
-
-		// First frame, give the groundtruth to the tracker
-		if (nFrames == 0) {
-			tracker.init( Rect(xMin, yMin, width, height), frame );
-			rectangle( frame, Point( xMin, yMin ), Point( xMin+width, yMin+height), Scalar( 0, 255, 255 ), 1, 8 );
-			resultsFile << xMin << "," << yMin << "," << width << "," << height << endl;
+		if(originalPoint!=processPoint&&!leftButtonDownFlag && !bInit)
+		{
+			x = originalPoint.x;
+			y = originalPoint.y;
+			width = processPoint.x - originalPoint.x;
+			height = processPoint.y - originalPoint.y;
+			// First frame, give the groundtruth to the tracker
+			tracker.init( Rect(x, y, width, height), frame );
+			rectangle( frame, Point( x, y ), Point( x+width, y+height), Scalar( 0, 255, 255 ), 1, 8 );
+			resultsFile << x << "," << y << "," << width << "," << height << endl;
+			bInit = true;
 		}
-		// Update
-		else{
+			// Update
+		if(bInit)
+		{
 			result = tracker.update(frame);
 			rectangle( frame, Point( result.x, result.y ), Point( result.x+result.width, result.y+result.height), Scalar( 0, 255, 255 ), 1, 8 );
 			resultsFile << result.x << "," << result.y << "," << result.width << "," << result.height << endl;
 		}
-
-		nFrames++;
-
 		if (!SILENT){
-			imshow("Image", frame);
+			imshow("KCF", frame);
 			waitKey(1);
 		}
 	}
 	resultsFile.close();
-
-	listFile.close();
 
 }
